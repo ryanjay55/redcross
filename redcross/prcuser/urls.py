8d76f7692601b5dd9b9e7acfccff13635b505d05
Ryan Jay D. Antonio <88523304+ryanjay55@users.noreply.github.com>
2023-03-20 21:57:00 +0800

    updated 03-20-23

--- redcross/prcuser/urls.py
+++ redcross/prcuser/urls.py
@@ -3,4 +3,8 @@ from prcuser import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('login', views.login, name='login'),
+  path('home', views.home, name='home'),
+  path('signup', views.signup, name='signup'),
+  
 ]
--- redcross/prcuser/views.py
+++ redcross/prcuser/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render (request, 'prcuser/index.html')
+
+def home(request):
+    return render (request, 'prcuser/index.html')
+
+def login(request):
+    return render(request, 'prcuser/userlogin.html')
+
+def signup(request):
+    return render(request, 'prcuser/usersignup.html')
